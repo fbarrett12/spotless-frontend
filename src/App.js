d3e8c9bbc496a177641d683884949c3c18fa444b
FredrickBarrett <f.barrett12@gmail.com>
2019-11-22 13:02:08 -0500

    used hooks to set state with fetched data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
+import { useSelector,useDispatch } from 'react-redux'
 
 import './App.css';
 
 function App() {
-  
+  const laundromats = useSelector(state => state.laundromats)
+  const dispatch = useDispatch()
+
   useEffect( () => {
     fetch("http://localhost:3000/providers")
     .then(res => res.json())
     .then(data => {
-      console.log(data)
+      dispatch({ type: 'LOAD_LAUNDROMATS', data: data})
     })
-  }, [])
+  }, [dispatch])
 
+  console.log(laundromats)
   return (
     <div className="App">
       
---
+++ src/reducers/reducer.js
@@ -0,0 +1,15 @@
+let defaultState = {
+    laundromats : []
+}
+
+export default function reducer(state = defaultState, action) {
+    switch(action.type) {
+        case 'LOAD_LAUNDROMATS' :
+            return {
+                ...state,
+                laundromats: action.data
+            }
+        default:
+            return state
+    }
+}
