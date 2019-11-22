e22bc3515a53f86671bef32210d98bf5436fd4f1
FredrickBarrett <f.barrett12@gmail.com>
2019-11-22 11:13:16 -0500

    set up useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import './App.css';
 
 function App() {
+  
+  useEffect( () => {
+    fetch("http://localhost:3000/providers")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  }, [])
+
   return (
     <div className="App">
       
